5531ce1cd1072fd573355ebd55736e14876e562e
jimtoepel <jim.toepel@gmail.com>
2016-01-06 21:39:08 -0800

    Finished if statements

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var population: Int = 5422
+var message: String
+var hasPostOffice: Bool = false
+
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else {
+    message = "\(population) is pretty big!"
+}
+
+print(message)
+
+if !hasPostOffice {
+    print("Where do we buy stamps?")
+}
