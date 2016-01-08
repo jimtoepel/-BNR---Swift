c1a67eb48114fe988420ff2623939e5212a73f9e
jimtoepel <jim.toepel@gmail.com>
2016-01-07 20:00:27 -0800

    NUMBERS

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+print("The maximu INT value is \(Int.max).")
+print("The minimum INT value is \(Int.min).")
+print("The maximum size for a 32-bit INT is \(Int32.max).")
+print("The minimum size for a 32-bit INT is \(Int32.min).")
+
+print("The maximum size of an unsigned INT is \(UInt.max).")
+print("The minimum size of an unsigned INT is \(UInt.min).")
+print("The maximum size of an 32-bit unsigned INT is \(UInt32.max).")
+print("The minimum size of an 32-bit unsigned INT is \(UInt32.min).")
+
+let numberOfPages: Int = 10 // declares the type explicitely
+let numberOfChapters = 3 // Also of type Int, but inferred by the compiler
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
