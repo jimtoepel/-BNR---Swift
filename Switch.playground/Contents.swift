cdc74e9f2a1f8b54cd9d69e001ed26966d559db2
jimtoepel <jim.toepel@gmail.com>
2016-01-10 16:37:41 -0800

    switches are still great.

--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -5,10 +5,22 @@ import Cocoa
 var statusCode: Int = 404
 var errorString: String = "The request failed with the error:"
 switch statusCode {
+case 100, 101:
+    errorString += " Informational, 1xx."
 
-case 400, 401, 403, 404:
-    errorString = "There was something wrong with the request."
-    fallthrough
+case 204:
+    errorString += " Successfu; but no content, 204."
+
+case 300...307:
+    errorString += " Redirection, 3xx."
+
+case 400...417:
+    errorString += " Cliet error, 4xx."
+    
+case 500...505:
+    errorString += " Server error, 5xx."
+    
 default:
-    errorString += " Please review the request and try again."
+    errorString = "Unknown. Please review the request and try again."
+    
 }
