baa3ffd40aed8234c09f41c600eadd6bb4dadb63
jimtoepel <jim.toepel@gmail.com>
2016-01-15 14:46:11 -0800

    loops and such.

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -4,7 +4,11 @@ import Cocoa
 
 var myFirstInt: Int = 0
 
-for i in 1...5 {
+for _ in 1...5 {
     ++myFirstInt
     print(myFirstInt)
+}
+
+for case let i in 1 ... 100 where i % 3 == 0 {
+    print (i)
 }
