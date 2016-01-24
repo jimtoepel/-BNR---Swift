77658138ce2901cd3c4548737a1e7693c56ec043
jimtoepel <jim.toepel@gmail.com>
2016-01-24 14:24:34 -0800

    Check in.
    YES

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -9,7 +9,7 @@ var newItems = [
     "Watch Lord of the Rings in a day",
     "Go on a walkabout",
     "Scuba dive in the Great Blue Hole",
-    "Find a Tripple Rainbow"
+    "Find a Triple Rainbow"
 ]
 
 bucketList += newItems
@@ -22,7 +22,7 @@ bucketList.removeAtIndex(2)
 print(bucketList.count)
 print(bucketList[0...2])
 
-bucketList[2] += " in Austrailia"
+bucketList[2] += " in Australia"
 bucketList
 
 print(bucketList[2])
@@ -34,3 +34,37 @@ bucketList.insert("Toboggan across Alaska", atIndex: 2)
 print(bucketList)
 
 
+var myronsList = [
+    "Climb Mt. McKinley",
+    "Fly hot air ballon in Fiji",
+    "Toboggan across Alaska",
+    "Go on a walkabout in Australia",
+    "Scuba dive in the Great Blue Hole",
+    "Find a Triple Rainbow"
+    ]
+
+let equal = (bucketList == myronsList)
+
+let lunches = [
+    "Cheeseburger",
+    "Veggie Pizza",
+    "Chicken Caesar Salad",
+    "Black Bean Burrito",
+    "Falafel wrap"
+]
+
+var toDoList = ["Take our garbage", "Pay bills", "Cross off finished items"]
+var goodWay:[String] = toDoList.reverse()
+
+var n = toDoList.count
+
+
+// Get the number of items in the list
+// Remove the last object, insert it at the I spot.
+
+for var i = 0; i < n; i++ {
+    toDoList.insert(toDoList.removeLast(), atIndex: i)
+}
+
+print (toDoList)
+print (goodWay)
