93d7972aed6f615460ea9988b5dccd407f559128
jimtoepel <jim.toepel@gmail.com>
2016-01-24 21:22:25 -0800

    Building a dictionary

---
+++ dictionaries.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+
+var movieRatings = ["Donnie Darko": 4, "Chungking Express": 5, "Dark City": 4]
+
+print("I have rated \(movieRatings.count) movies.")
+
+let darkoRating = movieRatings["Donnie Darko"]
+
+movieRatings["Dark City"] = 5
+movieRatings
+
+// optionals!
+
+let oldRating: Int? = movieRatings.updateValue(5, forKey: "Donnie Darko")
+if let lastRating = oldRating, currentRating = movieRatings["Donnie Darko"] {
+    print("Old rating: \(lastRating); current rating: \(currentRating)")
+}
+
+movieRatings["The Cabinet of Dr. Caligari"] = 5
+
+movieRatings.removeValueForKey("Dark City")
+
+movieRatings["Dark City"] = nil
+
