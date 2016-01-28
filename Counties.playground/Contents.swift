c6c9cf076c1902054c54edbde0f9fd485118fcbc
jimtoepel <jim.toepel@gmail.com>
2016-01-27 21:24:38 -0800

    ZIPS
    finished

--- Counties.playground/Contents.swift
+++ Counties.playground/Contents.swift
@@ -3,3 +3,14 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+let orangeZips =    [28903, 20393, 20134, 39393, 39218]
+let laZips =        [98209, 98780, 87689, 00848, 37478]
+let sfZips =        [30029, 39920, 02039, 85859, 32020]
+
+var countyZips = ["Orange" : orangeZips, "LA": laZips, "SF": sfZips]
+
+let allZips = Array(countyZips.values)
+
+
+print("California has the following zip codes: \(allZips)")
