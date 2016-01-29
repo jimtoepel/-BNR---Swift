247141b61fec7ba1c18c987420270cee84d400e5
jimtoepel <jim.toepel@gmail.com>
2016-01-28 21:39:25 -0800

    starting on sets.
    wish I was actually building something.

---
+++ Groceries.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var groceryBag = Set(["Apples", "Oranges", "Mangos"])
+
+
+groceryBag.insert("Bananas")
+
+for food in groceryBag {
+    print(food)
+}
