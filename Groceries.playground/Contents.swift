88e0af922c5abc6e3fb02b765057c41c2b221246
jimtoepel <jim.toepel@gmail.com>
2016-01-30 10:35:27 -0800

    Finished Sets + Started Challenge
    push some more later. Press goes now.

--- Groceries.playground/Contents.swift
+++ Groceries.playground/Contents.swift
@@ -10,4 +10,16 @@ for food in groceryBag {
     print(food)
 }
 
-let hasApples = groceryBag.contains("Tacos")
+let hasTacos = groceryBag.contains("Tacos")
+
+let friendsGroceryBag = Set(["Tacos", "Guac", "Milk", "Oranges"])
+let commonGroceryBag = groceryBag.union(friendsGroceryBag)
+
+let hasCommonTacos = commonGroceryBag.contains("Tacos")
+
+let roomatesGroceryBag = Set(["Apples", "Bananas", "Cereal", "Guac", "Toothpaste"])
+let itemsToReturn = commonGroceryBag.intersect(roomatesGroceryBag)
+
+let yourSecondBag = Set(["Berries", "Yogurt"])
+let roommatesSecondBag = Set(["Grapes", "Honey"])
+let disjoint = yourSecondBag.isDisjointWith(roommatesSecondBag)
