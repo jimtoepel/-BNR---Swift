209c78db366758a97d9fe0859a10924a39b66224
jimtoepel <jim.toepel@gmail.com>
2016-02-05 19:49:37 -0800

    phoned in. dig more tomorrow.

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -2,5 +2,10 @@
 
 import Cocoa
 
-var volunteerCount = [1, 3, 40, 32, 2, 53, 77, 13]
+var volunteerCounts = [1, 3, 40, 32, 2, 53, 77, 13]
 
+func sortAscending(i: Int, j: Int) -> Bool {
+    return i < j
+}
+
+let volunteersSorted = volunteerCounts.sort(sortAscending)
