1ea66f2fbca705a470bc4e86c6d944de8c80b5f2
jimtoepel <jim.toepel@gmail.com>
2016-02-09 21:31:23 -0800

    Started enums
    I really like these, they are toepel friendly switch statements.

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+enum TextAlignment {
+    case Left
+    case Right
+    case Center
+}
+
+var alignment = TextAlignment.Left
+alignment = .Center
+
+
+switch alignment {
+case .Left:
+    print("left aligned")
+    
+case .Right:
+    print("right aligned")
+    
+case .Center:
+    print("center aligned")
+    
+}
