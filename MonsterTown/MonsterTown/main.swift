8ff6bc3a18c926d37b5664e38a9890b331259e35
jimtoepel <jim.toepel@gmail.com>
2016-02-15 20:05:26 -0800

    Added monster town`
    A real project, finally!

---
+++ MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Jim Toepel on 2/15/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
