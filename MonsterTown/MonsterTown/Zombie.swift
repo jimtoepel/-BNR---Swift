16fceee9cb2da494f314ce2acda7725fd5e11a9d
jimtoepel <jim.toepel@gmail.com>
2016-02-19 10:38:01 -0800

    Plan to execute switch.

--- MonsterTown/MonsterTown/Zombie.swift
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -13,6 +13,12 @@ class Zombie: Monster {
     var walksWithLimp = true
     
     final override func terrorizeTown() {
+        
+        // switch to check town population
+        // Greater than ten? -> current flow
+        // 10 or less? -> print "killed everyone, nobody left", set to 0
+        // 0 -> "hungry zombie..."
+        
         town?.changePopulation(-10)
         super.terrorizeTown()
     }
