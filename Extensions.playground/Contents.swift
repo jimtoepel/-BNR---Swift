4d2d8810759adcbf9bb51e8d745805c0c351fccc
jimtoepel <jim.toepel@gmail.com>
2016-05-08 15:08:32 -0700

    Extended Int to include computed property
    Five times works!

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -48,3 +48,55 @@ extension Car {
 }
 
 var c = Car(carMake: "Ford", carModel: "Fusion", carYear: 2013)
+
+
+extension Car {
+    enum CarKind: CustomStringConvertible {
+        case Coupe, Sedan
+        var description: String {
+            switch self {
+            case .Coupe:
+                return "Coupe"
+            case .Sedan:
+                return "Sedan"
+            }
+        }
+    }
+    var kind: CarKind {
+        if numberOfDoors == 2 {
+            return .Coupe
+        } else {
+            return .Sedan
+        }
+    }
+}
+
+c.kind.description
+
+
+extension Car {
+    mutating func emptyGas(amount: Double) {
+        precondition(amount <= 1 && amount > 0, "Amount to remove must be beteen 0 and 1.")
+        gasLevel -= amount
+    }
+    
+    mutating func fillGas() {
+        gasLevel = 1.0
+    }
+}
+
+c.emptyGas(0.3)
+c.gasLevel
+c.fillGas()
+c.gasLevel
+
+
+extension Int {
+    var timesFive: Int { return self * 5 }
+}
+
+5.timesFive
+1.timesFive
+100.timesFive
+
+
