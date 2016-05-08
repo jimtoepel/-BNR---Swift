0cc4ffcc05f58cd2da6c5854fb5e4c69779c0ec6
jimtoepel <jim.toepel@gmail.com>
2016-05-08 10:37:58 -0700

    Added Struct

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -16,3 +16,16 @@ protocol VehicleType {
     var hasFlatBed: Bool { get }
 }
 
+
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    let color: String
+    let nickname:String
+    var gasLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1.")
+        }
+    }
+}
