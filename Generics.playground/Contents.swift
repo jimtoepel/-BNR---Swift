a248d3b4f843cb76fd73c9e3e9ed7c11bd54a6de
jimtoepel <jim.toepel@gmail.com>
2016-05-12 16:17:55 -0700

    Generic Arrays

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -2,17 +2,37 @@
 
 import Cocoa
 
-struct Stack {
-    var items = [Int]()
+struct Stack<Element> {
+    var items = [Element]()
     
-    mutating func push(newItem: Int) {
+    mutating func push(newItem: Element) {
         items.append(newItem)
     }
     
-    mutating func pop() -> Int? {
+    mutating func pop() -> Element? {
         guard !items.isEmpty else {
             return nil
         }
         return items.removeLast()
     }
 }
+
+
+
+var intStack = Stack<Int>()
+
+
+intStack.push(1)
+intStack.push(2)
+
+print(intStack.pop())
+print(intStack.pop())
+print(intStack.pop())
+
+
+var stringStack = Stack<String>()
+stringStack.push("this is a string")
+stringStack.push("another string")
+
+print(stringStack.pop())
+
