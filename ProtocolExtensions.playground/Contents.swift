04f155c0674e93afeb3cd31d1293c4b83d30d3e5
jimtoepel <jim.toepel@gmail.com>
2016-05-22 14:04:50 -0400

    Set up some generic Protocols and Extensions.

---
+++ ProtocolExtensions.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+protocol ExerciseType {
+    var name: String { get}
+    var caloriesBurned: Double { get }
+    var minutes: Double { get }
+    
+}
+
+
+struct ElipticalTrainer: ExerciseType {
+    let name = "Eliptical Machine"
+    let caloriesBurned: Double
+    let minutes: Double
+}
+
+let ElipticalWorkout = ElipticalTrainer(caloriesBurned: 335, minutes: 30)
+
+struct Treadmill: ExerciseType {
+    let name = "Treadmill"
+    let caloriesBurned: Double
+    let minutes: Double
+    let distanceInMiles: Double
+}
+
+let runningWorkout = Treadmill(caloriesBurned: 350, minutes: 25, distanceInMiles: 4.2)
+
