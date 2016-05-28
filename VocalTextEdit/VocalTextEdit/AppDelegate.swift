064e1c9d36c3a4a8e557ae2cd62985dc7a139a6a
jimtoepel <jim.toepel@gmail.com>
2016-05-28 16:58:04 -0700

    Created penultimate project
    This is a text editor that also reads the text back to you using the
    system functions.

---
+++ VocalTextEdit/VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by Jim Toepel on 5/28/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Jim Toepel on 5/28/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
