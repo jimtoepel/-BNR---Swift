9579d57deaee1e9e8e58cbd5c72b2a8877623163
jimtoepel <jim.toepel@gmail.com>
2016-05-31 15:48:59 -0700

    cleaned up the MVC structure

--- VocalTextEdit/VocalTextEdit/Document.swift
+++ VocalTextEdit/VocalTextEdit/Document.swift
@@ -10,10 +10,7 @@ import Cocoa
 
 class Document: NSDocument {
 
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
+    var contents: String = ""
 
     override class func autosavesInPlace() -> Bool {
         return true
@@ -23,22 +20,34 @@ class Document: NSDocument {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
+        
+        let viewController = windowController.contentViewController as! ViewController
+        viewController.contents = contents
         self.addWindowController(windowController)
     }
 
     override func dataOfType(typeName: String) throws -> NSData {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let windowController = windowControllers[0]
+        let viewController = windowController.contentViewController as! ViewController
+        
+        let contents = viewController.contents ?? ""
+        
+        if let data = contents.dataUsingEncoding(NSUTF8StringEncoding) {
+            return data
+        } else {
+            let userInfo = [NSLocalizedRecoverySuggestionErrorKey: "File cannot be encoded in UTF-8."]
+            throw NSError(domain: "com.bignerdrand.VocalTextEdit", code: 0, userInfo: userInfo)
+        }
+
     }
 
     override func readFromData(data: NSData, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        if let contents = NSString(data: data, encoding: NSUTF8StringEncoding) as? String {
+            self.contents = contents
+        } else {
+            let userInfo = [NSLocalizedRecoverySuggestionErrorKey: "File is not valid UTF-8."]
+            throw NSError(domain: "com.ignerdranch.VocalTextEdit", code: 0, userInfo: userInfo)
+        }
     }
-
-
 }
 
