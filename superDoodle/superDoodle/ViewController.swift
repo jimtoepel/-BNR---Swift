70a73c8c2b008d7d7ca047b555db557f7975076e
jimtoepel <jim.toepel@gmail.com>
2016-06-04 20:35:39 -0700

    Hooked up all of the elements

--- superDoodle/superDoodle/ViewController.swift
+++ superDoodle/superDoodle/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var itemTextField: UITextField!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        print("Add to-do item: \(itemTextField.text)")
     }
 
 
