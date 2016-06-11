9399eeb971a017965d3aeab79d55eec0a0bb7a5b
jimtoepel <jim.toepel@gmail.com>
2016-06-10 21:43:01 -0700

    Started final chapter

---
+++ Contacts/Contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Jim Toepel on 6/10/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts/Contacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by Jim Toepel on 6/10/16.
+//  Copyright © 2016 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
